bb5bb470d1aa45094ca7109e36ccb81db7003eb5
HoloborodkoMykhailo <71754811+HoloborodkoMykhailo@users.noreply.github.com>
2020-11-18 22:40:47 +0200

    Create ЛБ2++.cpp

---
+++ ЛБ2++.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t, z;
+	cout << "t = "; cin >> t;
+	z = t % 6;  // знаходимо остачу від ділення 
+	if ((z == 1)||(z == 2)||(z == 3))
+	{
+		cout << "Green light\n";
+  }
+	else
+	{
+		if (z == 4)
+		{
+			cout << "Yellow light\n";
+		}
+		else
+		{
+			cout << "Red light\n";
+		}
+	}
+	system("pause");
+}
